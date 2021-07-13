235dace1a7e1ff934f594fed38d254ae8b434f94
Lakshya Singh Lalotra <68769782+Lakshya-Lalotra@users.noreply.github.com>
2021-07-13 18:10:06 +0530

    SITE1

---
+++ app/main.js
@@ -0,0 +1,53 @@
+$(document).ready(function(){
+
+  $('.fa-bars').click(function(){
+    $(this).toggleClass('fa-times');
+    $('.nav').toggleClass('nav-toggle');
+  });
+
+  $(window).on('load scroll',function(){
+
+    $('.fa-bars').removeClass('fa-times');
+    $('.nav').removeClass('nav-toggle');
+
+    if($(window).scrollTop() > 10){
+      $('header').addClass('header-active');
+    }else{
+      $('header').removeClass('header-active');
+    }
+
+  });
+
+  $('.facility').magnificPopup({
+    delegate:'a',
+    type:'image',
+    gallery:{
+      enabled:true
+    }
+  });
+
+});
+
+
+// Slide show
+
+var slideIndex = [1,1];
+/* Class the members of each slideshow group with different CSS classes */
+var slideId = ["mySlides1", "mySlides2"]
+showSlides(1, 0);
+showSlides(1, 1);
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+function showSlides(n, no) {
+  var i;
+  var x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex[no]-1].style.display = "block";
+}
